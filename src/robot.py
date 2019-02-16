62c85719e69dbac0623b680a05cfdeac9b851772
James <jsokoll@ford.com>
2019-02-15 23:08:00 -0500

    Added Arm subsystem and commands

--- src/robot.py
+++ src/robot.py
@@ -5,10 +5,12 @@ from commands.do_nothing import DoNothing
 from oi import OI
 from subsystems.drivetrain import Drivetrain
 from subsystems.panel_grabber import PanelGrabber
+from subsystems.arm import Arm
 
 class MyRobot(wpilib.IterativeRobot):
     oi = None
     drivetrain = None
+    arm = None
     panel_grabber = None
     autonomous_command = None
 
@@ -49,6 +51,7 @@ class MyRobot(wpilib.IterativeRobot):
         """
         self.oi = OI(self)
         self.drivetrain = Drivetrain(self)
+        self.arm = Arm(self)
         self.panel_grabber = PanelGrabber(self)
         self.oi.setup_button_bindings()
         # wpilib.CameraServer.launch()
