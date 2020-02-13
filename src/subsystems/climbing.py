da84bec13d14f274830803995ffba4b5d200a737
Aaron DeGrow <account+github@aarondegrow.com>
2020-02-13 12:33:19 -0500

    Basic subsystem for climbing

--- src/subsystems/climbing.py
+++ src/subsystems/climbing.py
@@ -33,20 +33,35 @@ class Climbing(Subsystem):
     def _init_components(self):
         self._max_speed = self._config.getfloat(Climbing.GENERAL_SECTION, Climbing.MAX_SPEED_KEY)
         if self._config.getboolean(Climbing.GENERAL_SECTION, Climbing.ENABLED_KEY):
-            self._motor = PWMTalonSRX(self._config.getint(self.GENERAL_SECTION, Climbing.CHANNEL_KEY))
+            self._motor = PWMTalonSRX(self._config.getint(Climbing.GENERAL_SECTION, Climbing.CHANNEL_KEY))
             self._motor.setInverted(self._config.getboolean(Climbing.GENERAL_SECTION, Climbing.INVERTED_KEY))
         if self._config.getboolean(Climbing.LIMIT_SWITCH_SECTION, Climbing.ENABLED_KEY):
-            self._limit_switch = DigitalInput(self._config.getint(self.LIMIT_SWITCH_SECTION, Climbing.CHANNEL_KEY))
-
-    def _update_smartdashboard_sensors(self):
-        pass
-        if self._limit_switch is not None:
-            SmartDashboard.putBoolean("Climbing Switch", line_state[1])
-
-        #SmartDashboard.putNumber("Drivetrain Sonar Distance", self._sonar_distance)
+            self._limit_switch = DigitalInput(self._config.getint(Climbing.LIMIT_SWITCH_SECTION, Climbing.CHANNEL_KEY))
 
     def initDefaultCommand(self):
         pass
-#        self.setDefaultCommand(TankDrive(self._robot, 'TankDrive', modifier_scaling=self._modifier_scaling,
-#                                         dpad_scaling=self._dpad_scaling))
+    #        self.setDefaultCommand(TankDrive(self._robot, 'TankDrive', modifier_scaling=self._modifier_scaling,
+    #                                         dpad_scaling=self._dpad_scaling))
 
+    def is_retracted(self) -> bool:
+        if self._limit_switch is not None:
+            return self._limit_switch.get()
+        else:
+            return False
+
+    def _update_smartdashboard_sensors(self, speed: float):
+        SmartDashboard.putNumber("Climbing Speed", speed)
+        if self._limit_switch is not None:
+            SmartDashboard.putBoolean("Climbing Switch", self._limit_switch.get())
+
+    def move_winch(self, speed: float):
+        adjusted_speed = 0.0
+        if self._motor:
+            if speed < 0.0:
+                adjusted_speed = speed * self._max_speed
+            elif speed > 0.0 and not self.is_retracted():
+                adjusted_speed = speed * self._max_speed
+            else:
+                adjusted_speed = 0.0
+            self._motor.set(adjusted_speed)
+        self._update_smartdashboard(adjusted_speed)
