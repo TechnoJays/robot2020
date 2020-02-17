15d989d76eede6275cb8e780e387ab35733c3f26
Ty Strayer <tstrayer@nustep.com>
2020-02-17 16:27:45 -0500

    Added extender solenoid and associated ini fields and methods

--- src/subsystems/control_panel.py
+++ src/subsystems/control_panel.py
@@ -2,6 +2,7 @@ import configparser
 from enum import Enum
 
 from wpilib._wpilib import PWMTalonSRX
+from wpilib._wpilib import Solenoid
 from wpilib.command import Subsystem
 from wpilib import I2C
 
@@ -34,9 +35,11 @@ class ControlPanel(Subsystem):
 
     COLOR_PROFILE_KEY = "COLOR_PROFILE"
     ENABLED_KEY = "ENABLED"
-    INVERTED_KEY = "INVERTED"
-    CHANNEL_KEY = "CHANNEL"
+    MOTOR_INVERTED_KEY = "MOTOR_INVERTED"
+    MOTOR_CHANNEL_KEY = "MOTOR_CHANNEL"
     MAX_SPEED_KEY = "MAX_SPEED"
+    SOLENOID_CHANNEL_KEY = "SOLENOID_CHANEL"
+    SOLENOID_INVERTED_KEY = "SOLENOID_INVERTED"
 
     R_KEY = "R"
     G_KEY = "G"
@@ -51,8 +54,11 @@ class ControlPanel(Subsystem):
     _robot = None
     _color_sensor: ColorSensorV3 = None
     _motor = None
+    _extender = None
+    _solenoid = None
 
     _max_speed = 0.0
+    _solenoid_inverted = False
 
     _red_target = None
     _green_target = None
@@ -67,26 +73,28 @@ class ControlPanel(Subsystem):
         self._load_color_profile()
 
     def _init_components(self):
-        self._max_speed = self._config.getfloat(self.GENERAL_SECTION, self.MAX_SPEED_KEY)
-        if self._config.getboolean(self.GENERAL_SECTION, self.ENABLED_KEY):
+        self._max_speed = self._config.getfloat(ControlPanel.GENERAL_SECTION, ControlPanel.MAX_SPEED_KEY)
+        self._solenoid_inverted = self._config.getboolean(ControlPanel.GENERAL_SECTION, ControlPanel.SOLENOID_INVERTED_KEY)
+        if self._config.getboolean(ControlPanel.GENERAL_SECTION, ControlPanel.ENABLED_KEY):
             self._color_sensor = ColorSensorV3(I2C.Port.k0nboard)
-            self._motor = PWMTalonSRX(self._config.getint(self.GENERAL_SECTION, self.CHANNEL_KEY))
-            self._motor.setInverted(self._config.getboolean(self.GENERAL_SECTION, self.INVERTED_KEY))
+            self._motor = PWMTalonSRX(self._config.getint(ControlPanel.GENERAL_SECTION, ControlPanel.CHANNEL_KEY))
+            self._motor.setInverted(self._config.getboolean(ControlPanel.GENERAL_SECTION, ControlPanel.INVERTED_KEY))
+            self._solenoid = Solenoid(self._config.getint(ControlPanel.GENERAL_SECTION, ControlPanel.SOLENOID_CHANNEL_KEY))
 
     def _load_color_profile(self):
         if self._config.getboolean(self.GENERAL_SECTION, self.ENABLED_KEY):
-            color_profile = self._config.get(self.GENERAL_SECTION, self.COLOR_PROFILE_KEY)
-            self._red_target = self._load_color_target(color_profile, self.RED_KEY)
-            self._blue_target = self._load_color_target(color_profile, self.BLUE_KEY)
-            self._yellow_target = self._load_color_target(color_profile, self.YELLOW_KEY)
-            self._green_target = self._load_color_target(color_profile, self.GREEN_KEY)
+            color_profile = self._config.get(ControlPanel.GENERAL_SECTION, ControlPanel.COLOR_PROFILE_KEY)
+            self._red_target = self._load_color_target(color_profile, ControlPanel.RED_KEY)
+            self._blue_target = self._load_color_target(color_profile, ControlPanel.BLUE_KEY)
+            self._yellow_target = self._load_color_target(color_profile, ControlPanel.YELLOW_KEY)
+            self._green_target = self._load_color_target(color_profile, ControlPanel.GREEN_KEY)
 
     def _load_color_target(self, profile, color):
         section = profile + color
-        r = self._config.getint(section, self.R_KEY)
-        g = self._config.getint(section, self.G_KEY)
-        b = self._config.getint(section, self.B_KEY)
-        tolerance = self._config.getint(section, self.TOLERANCE_KEY)
+        r = self._config.getint(section, ControlPanel.R_KEY)
+        g = self._config.getint(section, ControlPanel.G_KEY)
+        b = self._config.getint(section, ControlPanel.B_KEY)
+        tolerance = self._config.getint(section, ControlPanel.TOLERANCE_KEY)
         return ColorTarget(r, g, b, tolerance)
 
     def get_current_color(self):
@@ -113,6 +121,9 @@ class ControlPanel(Subsystem):
         elif speed > 1:
             speed = 1
         self._motor.set(speed * self._max_speed)
+
+    def extend(self, state):
+        self._solenoid.set(state and not self._solenoid_inverted)
 
 
     @staticmethod
